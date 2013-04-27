e0df9855871a8865df06fc65391410c17dd03a10
Ian W. Remmel <engineering@ianwremmel.com>
2013-04-27 09:49:16 -0400

    Added lodash dependency, made code work

--- tasks/customize-bootstrap.js
+++ tasks/customize-bootstrap.js
@@ -2,7 +2,7 @@
 
 module.exports = function (grunt) {
 	var fs = require('fs');
-	var _ = require('_');
+	var _ = require('lodash');
 
 	var requiredOptions = [
 		'components',
@@ -14,12 +14,14 @@ module.exports = function (grunt) {
 		var done = this.async();
 
 		_.each(requiredOptions, function(option) {
-			grunt.config.requires(['options', option]);
+			grunt.config.requires(['customizeBootstrap', 'options', option]);
 		});
 
-		var bootstrapPath = grunt.config('options.components') + 'components/bootstrap/less/';
+		var options = this.options();
 
-		fs.readDir(grunt.config('src'), function(err, overrides) {
+		var bootstrapPath = options.components + '/bootstrap/less/';
+
+		fs.readdir(options.src, function(err, overrides) {
 			if (err) {
 				done(false);
 			}
@@ -29,22 +31,24 @@ module.exports = function (grunt) {
 					done(false);
 				}
 
+				bootstrapLess = bootstrapLess.toString();
 				// First, replace each overridden item in the original bootstrap.less with the path to the
 				// its override.
 				_(overrides).each(function(filename) {
-					bootstrapLess = bootstrapLess.replace('"' + filename, '"' + grunt.config('src') + '/' + filename);
+					bootstrapLess = bootstrapLess.replace('"' + filename, '"' + options.src + '/' + filename);
 				});
 
 				// Now, adjust the paths on the non-overridden files so that the less
 				// compiler can find them from the location of the new bootstrap.less.
 				// We make sure we only match items inside quotes that do not start with a leading .
 				var pattern = new RegExp('@import "(' + bootstrapPath + '[^.][\\w\\.\\-_]*?)"');
-				bootstrapLess = bootstrapLess.replace(pattern, '@import "' + grunt.config('dest') + '$1"');
+				// TODO need to traverse up directory tree when replacing include path
+				bootstrapLess = bootstrapLess.replace(pattern, '@import "' + options.dest + '$1"');
 
 				// Finally, write the new bootstrap.less to the dest directory.
-				fs.exists(grunt.config('dest'), function(exists) {
+				fs.exists(options.dest, function(exists) {
 					if (exists) {
-						fs.writeFile(grunt.config('dest') + '/bootstrap.less', bootstrapLess, function(err) {
+						fs.writeFile(options.dest + '/bootstrap.less', bootstrapLess, function(err) {
 							if (err) {
 								done(false);
 							}
@@ -52,12 +56,12 @@ module.exports = function (grunt) {
 						});
 					}
 					else {
-						fs.mkdir(grunt.config('dest'), function(err) {
+						fs.mkdir(options.dest, function(err) {
 							if (err) {
 								done(false);
 							}
 
-							fs.writeFile(grunt.config('dest') + '/bootstrap.less', bootstrapLess, function(err) {
+							fs.writeFile(options.dest + '/bootstrap.less', bootstrapLess, function(err) {
 								if (err) {
 									done(false);
 								}
